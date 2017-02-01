96818ace7bcf767f8dfbc51ef564ef7d7bc1e429
es-lee <dldms800@gmail.com>
2017-02-02 01:22:19 +0900

    check result of google search by image with requests

--- imgsearch.py
+++ imgsearch.py
@@ -1,5 +1,6 @@
 import base64
 import os
+import requests
 
 def save_img(img, filename):
     img_bytes = img.encode('ascii')
@@ -10,3 +11,15 @@ def save_img(img, filename):
 
 def remove_img(filename):
     os.remove(filename)
+
+def img_search(filename):
+    search_url = 'http://images.google.com/searchbyimage?site=search&image_url='
+    img_url = 'oil.kkyung.com/imgs/' + filename
+    url = search_url + img_url
+    response = requests.get(url)
+    response = requests.get(response.url)
+    f = open('resp.txt', 'w')
+    f.write(response.text)
+    f.close()
+
+img_search('a.jpg')
