97c5aff1f5396ccdf07c1ce59130bee2052c5f98
System Administrator <root@Jangsoheeui-MacBook-Pro.local>
2017-02-13 01:00:38 +0900

    id, comment, url 추출

--- crawler.py
+++ crawler.py
@@ -1,17 +1,25 @@
 import requests
+import pymysql
+import sys
 from bs4 import BeautifulSoup
 
+def climber_crawler(celeb):
+   #max_pages
+   # page = 1
+   # while page <= max_pages:
+        url = 'https://search.naver.com/search.naver?where=realtime&sm=tab_jum&ie=utf8&query=' + celeb
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-def climber_crawler(max_pages):
-    page = 1
-    while page <= max_pages:
-        url = 'https://search.naver.com/search.naver?where=realtime&sm=tab_jum&ie=utf8&query=송중기'
-        source_code = requests.get(url, allow_redirects=False)
-        plain_text = source_code.text
-        soup = BeautifulSoup(plain_text,'html.parser')
-        for comments in soup.findAll('a', {'class': 'txt_link'}):
-            print(comments.get_text())
-        for comments in soup.findAll('span', {'class': 'cmmt _twitter'}):
-            print(comments.get_text())
-        page += 1
-climber_crawler(1)
+        #a tag, txt_link
+        a_tags = soup.find_all('a')
+        for a in a_tags:
+            a_class = a.get('class')
+            if a_class != None and a_class[0] == 'rt_user':
+                user = a.get_text()
+            if a_class != None and a_class[0] == 'txt_link':
+                comment = a.get_text()
+                url = a.get('href')
+                print(user, comment, url)
+
+climber_crawler('송중기')
