1ebee8a959f703574110e5958c3e91fceb037ebc
es-lee <dldms800@gmail.com>
2017-02-20 17:29:33 +0900

    modify oil.thrift & server imgSearch handler

--- oil.thrift
+++ oil.thrift
@@ -1,24 +1,6 @@
 namespace java oil_climber
 
-struct SimilarImg {
-  1: string imgUrl,
-  2: string linkUrl
-}
-
-struct MatchingPage {
-  1: string title,
-  2: string imgUrl,
-  3: string linkUrl,
-  4: string content
-}
-
-struct ImgSearchResult {
-  1: string guess,
-  2: list<SimilarImg> imgs,
-  3: list<MatchingPage> pages
-}
-
 service Oil {
-    ImgSearchResult imgSearch(1:string img)
+    string imgSearch(1:string img)
 }
 
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import thriftpy
 oil_thrift = thriftpy.load("oil.thrift", module_name="oil_thrift")
 
 from thriftpy.rpc import make_server
+import imgsearch
 
 class OilHandler:
     def __init__(self):
@@ -12,28 +13,24 @@ class OilHandler:
         print("ping()")
 
     def imgSearch(self, img):
-        img_file = open(self.get_img_title(), "w")
-        #img_file.write(base64.b64decode(img))
-        img_file.close()
+        imgsearch.remove_img(self.get_prev_filename())
+        print("Image Search : previous image deleted")
+
+        filename = self.get_new_filename()
+        imgsearch.save_img(img, filename)
         print("Image Search : image saved")
 
-        similar = oil_thrift.SimilarImg("http://image.hankookilbo.com/i.aspx?Guid=5f5962508457411cb1882a0747acb9a0&Month=201603&size=640","http://m.naver.com")
-        page = oil_thrift.MatchingPage("title", "http://image.hankookilbo.com/i.aspx?Guid=5f5962508457411cb1882a0747acb9a0&Month=201603&size=640", "http://m.naver.com", "content")
+        imgsearch.make_url(filename)
+        print("Image Search : url generated")
 
-        result = oil_thrift.ImgSearchResult()
-        print("Image Search : gen result")
-        result.guess = "와왕 최유정 직찍"
-        result.imgs = [similar, similar, similar, similar, similar]
-        print("Image Search : fill imgs")
-        result.pages = [page, page, page, page, page]
-        print("Image Search : fill pages")
-
-        print("Image Search : return result")
         return result
 
-    def get_img_title(self):
+    def get_new_filename(self):
         self.img_title += 1
-        return repr(self.img_title)+".bmp"
+        return "~/www/imgs/" + repr(self.img_title) + ".bmp"
+
+    def get_prev_filename(self):
+        return "~/www/imgs/" + repr(self.img_title) + ".bmp"
 
 server = make_server(oil_thrift.Oil, OilHandler(), '0.0.0.0', 9090)
 print("starting oil server...")
